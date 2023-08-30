3f7bfe092724fa4e6035cc4621caedb35ac41f44
Sammy Barasa <barsamms@gmail.com>
2023-08-30 14:10:02 +0300

    docs: added swagger configuratoin

---
+++ src/main/java/com/munywele/cards/config/ApiDocsConfig.java
@@ -0,0 +1,8 @@
+package com.munywele.cards.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class ApiDocsConfig {
+
+}
